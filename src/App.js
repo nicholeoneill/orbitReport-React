44460bbf24e1cf1673c8c4c5be8ff442eabbc4d5
Nichole O'Neill <nazm612@gmail.com>
2023-08-10 18:58:37 -0500

    added Banner component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import { Banner } from "./components/Banner";
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
-
+import useState from "react";
+import satData from "./components/satData";
 
 function App() {
+  const [sat, setSat] = useState(satData);
+  const displaySats = [...new Set(satData.map((data) => data.orbitType))];
+
+  const filterByType = (curretnType) => {
+    const displaySats = satData.filter((newSatDisplay) => {
+      return newSatDisplay.orbitType === curretnType;
+    });
+    setSat(displaySats);
+  }
+
   return (
     <div>
-      <Buttons />
-      <Table />
+      <Banner />
+      <Buttons 
+        filterByType={filterByType}
+        setSat={setSat}
+        displaySats={displaySats}
+      />
+      <Table sat={sat} />
     </div>
   );
 }
---
+++ src/components/Banner.js
@@ -0,0 +1,8 @@
+export function Banner () {
+    return (
+        <header>
+            <p>Click on the buttons to see the satellites and the orbit type</p>
+        </header>
+        
+    );
+}
