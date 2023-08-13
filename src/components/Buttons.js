23cca0159783a98699ac4b4c97a391debcab86c2
Nichole O'Neill <nazm612@gmail.com>
2023-08-12 19:51:33 -0500

    made changes to Buttons.js

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,22 +1,20 @@
 import satData from "./satData";
 import styles from "./styling.css";
 
-const Buttons = (filterByType, setSat, displaySats) => {
+const Buttons = ({filterByType, setSat, displaySats}) => {
   // console.log(filterByType);
   // console.log(setSat);
-  console.log(displaySats);
-  // console.log(satData);
+  // console.log(displaySats);
+  
   return (
     <div className={styles.flexContainer}>
-      {displaySats
-        ? displaySats.map((sat, id) => {
+      {displaySats.map((sat, id) => {
             return (
               <button onClick={() => filterByType(sat)} key={id}>
                 {sat} Orbit
               </button>
             );
-          })
-        : null}
+        })}
       <button onClick={() => setSat(satData)}>All Orbits</button>
     </div>
   );
