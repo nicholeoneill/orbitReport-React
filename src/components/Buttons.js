eb32558d55f2613536e7033100a7a99bf83b546a
Nichole O'Neill <nazm612@gmail.com>
2023-08-12 20:23:49 -0500

    completed project

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -2,10 +2,6 @@ import satData from "./satData";
 import styles from "./styling.css";
 
 const Buttons = ({filterByType, setSat, displaySats}) => {
-  // console.log(filterByType);
-  // console.log(setSat);
-  // console.log(displaySats);
-  
   return (
     <div className={styles.flexContainer}>
       {displaySats.map((sat, id) => {
